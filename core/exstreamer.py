cda20bc54980d023c5949b5eebc24dc4a08aa466
Ben Demaree <bendemaree@gmail.com>
2012-04-25 19:35:03 -0500

    Converted mute to local-state model as well as volume.  Created basic server file.  Added disconnect() method.

--- core/exstreamer.py
+++ core/exstreamer.py
@@ -10,33 +10,74 @@ class Exstreamer:
         self.ip = ip
         self.port = port
         self.connection = None
+        self.volume = None
 
-    def get_volume(self):
+        self.connect()
+        self.set_mute(False)
+
+    def get_device_volume(self):
         if not self.is_connected():
             self.connect()
-
+        
         self.connection.send(wrap_command(CMD_GETVOLUME))
         raw_vol = self.connection.recv(BUFFER_SIZE)
         vol_xml = etree.fromstring(raw_vol) # create xml representation
         return int(vol_xml.text) * 5  # convert to percentage
 
+    def get_volume(self):
+        if not self.is_connected():
+            self.connect()
+        
+        return self.volume
+
     def set_volume(self, vol):
-        if not self.is_connected(): 
-            connect()
+        if not self.is_connected():
+            self.connect()
 
         # volume levels are defined 0 thru 20 (off thru max)
         # we will accept a volume percentage
         if(vol > 100 or vol < 0):
             raise InvalidVolumeLevel('That volume is out of range.')
-        vol = round_to_base(vol, 5) / 5  # convert to base 0 thru 20
-        print vol
-        self.connection.send(wrap_command(CMD_SETVOLUME, vol))
+        converted_vol = round_to_base(vol, 5) / 5  # convert to base 0 thru 20
+        self.connection.send(wrap_command(CMD_SETVOLUME, converted_vol))
         self.connection.recv(BUFFER_SIZE)  # receive and discard ACK
+        self.volume = vol
+
+    def get_mute(self):
+        if not self.is_connected():
+            self.connect()
+        
+        if not self.get_volume():
+            return True
+        return False
+
+    def set_mute(self, state):
+        if not self.is_connected():
+            self.connect()
+
+        if state:
+            self.connection.send(wrap_command(CMD_MUTEON))
+            self.connection.recv(BUFFER_SIZE)
+            self.volume = False
+        else:
+            self.connection.send(wrap_command(CMD_MUTEOFF))
+            self.connection.recv(BUFFER_SIZE)
+            self.volume = self.get_device_volume()
 
     def connect(self):
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         s.connect((self.ip, self.port))
         self.connection = s
+
+    def disconnect(self):
+        if(self.connection == None):
+            return True
+
+        self.connection.close()
+        self.connection = None
+
+        # make stored values stale
+        self.volume = None
 
     def is_connected(self):
         if self.connection is None:
--- core/util.py
+++ core/util.py
@@ -1,11 +1,38 @@
  #!/usr/bin/python
 
+import time
+
 DEFAULT_COMMAND_PORT = 12302
 BUFFER_SIZE = 1024
 
 CMD_GETVOLUME = 'L=volume.ack'
 CMD_SETVOLUME = 'v='
+CMD_MUTEON = 'c=40'
+CMD_MUTEOFF = 'c=41'
+CMD_GETCONFIG = 'L=getconfig.ack'
+
 CMD_TERMINATOR = '\n'
+
+def recvall(con, timeout=2):
+    con.setblocking(0)
+    total_data=[];
+    data='';
+    begin=time.time()
+    while 1:
+        if total_data and time.time()-begin>timeout:
+            break
+        elif time.time()-begin>timeout*2:
+            break
+        try:
+            data = con.recv(BUFFER_SIZE)
+            if data:
+                total_data.append(data)
+                begin=time.time()
+            else:
+                time.sleep(0.1)
+        except:
+            pass
+    return ''.join(total_data)
 
 def round_to_base(x, base=5):
     return int(base * round(float(x)/base))
